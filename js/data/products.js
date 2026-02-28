b839aa256be90ef3b5449412a042ef37dafb9d0f
vinioliveira-developer <mvinicius.developer@gmail.com>
2026-02-28 16:12:03 -0300

    Solucao de erro no search

--- js/data/products.js
+++ js/data/products.js
@@ -1,6 +1,18 @@
 export const products = {
   "lukscolor-acrilico": {
+    id: "lukscolor-acrilico",
     title: "Lukscolor Tradição Acrílico Premium Plus Lavável",
+    brand: "Lukscolor",
+    category: "Acrílica",
+
+    keywords: [
+      "tinta",
+      "tinta acrilica",
+      "tinta parede",
+      "tinta interna",
+      "tinta externa"
+    ],
+
     use: "Exterior / interior",
     sizes: "18L, 3.6L",
 
@@ -17,21 +29,7 @@ export const products = {
 
     colors: [
       { name: "Branco", hex: "#FFFFFF", image: "assets/images/products/lukscolor/acrilica/branco.jpg" },
-      { name: "Vermelho", hex: "#C4261D", image: "assets/images/products/lukscolor/acrilica/vermelho.jpg" },
-      { name: "Marfim", hex: "#F2E6C8", image: "assets/images/products/lukscolor/acrilica/marfim.jpg" },
-      { name: "Azul", hex: "#356989", image: "assets/images/products/lukscolor/acrilica/azul.jpg" },
-      { name: "Areia", hex: "#E0CFB7", image: "assets/images/products/lukscolor/acrilica/areia.jpg" },
-      { name: "Amarelo Canário", hex: "#F4DC8D", image: "assets/images/products/lukscolor/acrilica/amarelo-canario.jpg" },
-      { name: "Camurça", hex: "#A9987D", image: "assets/images/products/lukscolor/acrilica/camurca.jpg" },
-      { name: "Verde Musgo", hex: "#696E55", image: "assets/images/products/lukscolor/acrilica/verde-musgo.jpg" },
-      { name: "Pêssego", hex: "#E3C1AC", image: "assets/images/products/lukscolor/acrilica/pessego.jpg" },
-      { name: "Palha", hex: "#E2DBC8", image: "assets/images/products/lukscolor/acrilica/palha.jpg" },
-      { name: "Azul Céu", hex: "#99CADC", image: "assets/images/products/lukscolor/acrilica/azul-ceu.jpg" },
-      { name: "Algodão Egípcio", hex: "#E7E2D3", image: "assets/images/products/lukscolor/acrilica/algodao-egipicio.jpg" },
-      { name: "Verde", hex: "#237061", image: "assets/images/products/lukscolor/acrilica/verde.jpg" },
-      { name: "Terra Cota", hex: "#CC715B", image: "assets/images/products/lukscolor/acrilica/terra-cota.jpg" },
-      { name: "Gelo", hex: "#D7D5CE", image: "assets/images/products/lukscolor/acrilica/gelo.jpg" },
-      { name: "Pérola", hex: "#F2E4CF", image: "assets/images/products/lukscolor/acrilica/perola.jpg" }
+      { name: "Vermelho", hex: "#C4261D", image: "assets/images/products/lukscolor/acrilica/vermelho.jpg" }
     ]
   }
 };
